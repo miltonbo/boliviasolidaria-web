7151e4a7d48ff2d16f990f358aa8863da5e6838c
smeerkahoven <smeerkahoven@gmail.com>
2020-04-12 20:15:48 -0400

    revision formulario ayuda

--- ngx-admin/src/app/services/bolivia.solidaria.service.ts
+++ ngx-admin/src/app/services/bolivia.solidaria.service.ts
@@ -6,6 +6,7 @@ import { PuntosSolicitudesResponse } from '../data/response/solicitudes.response
 import { AyudaSolicitudRequest } from '../data/request/solicitudes.request';
 import { AyudaSolicitudResponse } from '../data/response/ayuda.solicitud.response';
 import 'rxjs/add/operator/catch';
+import { NbToastrService, NbToastrConfig, NbGlobalPosition, NbGlobalPhysicalPosition, NbComponentStatus } from '@nebular/theme';
 
 @Injectable({
     providedIn : 'root',
@@ -16,12 +17,37 @@ export class BoliviaSolidariaService {
 
     currentPosition = {lat : 0, lng : 0};
 
-    constructor(private http : HttpClient) {}
+    config: NbToastrConfig;
+
+    index = 1;
+    destroyByClick = true;
+    duration = 2000;
+    hasIcon = true;
+    position: NbGlobalPosition = NbGlobalPhysicalPosition.TOP_RIGHT;
+    preventDuplicates = false;
+    status: NbComponentStatus = 'primary';
+
+    constructor(private http : HttpClient, 
+        private toastrService: NbToastrService,) {}
     
+
+    showToast(type: NbComponentStatus, title: string, body: string, duration?) {
+        const config = {
+          status: type,
+          destroyByClick: this.destroyByClick,
+          duration: duration ? 3000 : duration,
+          hasIcon: this.hasIcon,
+          position: this.position,
+          preventDuplicates: this.preventDuplicates,
+        };
+        const titleContent = title ? `. ${title}` : '';
     
-    getData(): any[] {
-        throw new Error("Method not implemented.");
-    }
+        this.index += 1;
+        this.toastrService.show(
+          body,
+          `Toast ${this.index}${titleContent}`,
+          config);
+      }
 
     getObtenerListaSolicitudes() {
         return this.http.get<PuntosSolicitudesResponse>(`${this.baseUrl}/solicitudes/puntos`)
@@ -35,7 +61,27 @@ export class BoliviaSolidariaService {
 
     realizarSolicitud(request: AyudaSolicitudRequest) {
 
-        return this.http.post<AyudaSolicitudResponse>(`${this.baseUrl}/solicitudes`, request)
+        let headers = new HttpHeaders().set('Content-Type','application/json')
+            //.set('Access-Control-Request-Method', 'POST')
+           // .set('Access-Control-Request-Headers', 'content-type')
+        ;
+
+        return this.http.post<AyudaSolicitudResponse>(`${this.baseUrl}/solicitudes`, request, {headers : headers})
+            .pipe(
+                retry(3),
+                map((response: AyudaSolicitudResponse)=> {
+                    return response ;
+                }), 
+            )
+    }
+
+    enviarImagen(request: AyudaSolicitudResponse, file) {
+
+        const formData = new FormData();
+        formData.append('foto',file) ;
+        formData.append('key','foto') ;
+
+        return this.http.post<AyudaSolicitudResponse>(`${this.baseUrl}/solicitudes/${request.id}/ci-foto`, formData )
             .pipe(
                 retry(3),
                 map((response: AyudaSolicitudResponse)=> {
--- ngx-admin/src/environments/environment.prod.ts
+++ ngx-admin/src/environments/environment.prod.ts
@@ -8,4 +8,5 @@ export const environment = {
   googleApiKey : 'AIzaSyAN7dZ7k1C9_CJxrSgjs_okmVbrgMtlhkc' ,
   //urlWS :  'https://boliviasolidaria.com/bs-api',
   urlWS :  'http://localhost:4200/bs-api',
+  index : '/pages/content',
 };
